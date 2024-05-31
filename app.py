cdd084c575d83b24f05d9bd95925d8e1fdbf9784
adjeleyosekre <haosekre@gmail.com>
2024-06-01 02:52:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ product_list=df['Product Name'].unique
 
 total_sales_product=df[['Product Name','Sales']].groupby('Product Name').agg({'Sales':'sum'}).reset_index().sort_values(by='Sales',ascending=False)
 st.dataframe(total_sales_product.head())
-st.dataframe(total_sales_product,2)
-total_sales_product['Slaes']=round(total_sales_product['Sales'],2)
+##st.dataframe(total_sales_product,2)
+total_sales_product['Sales']=round(total_sales_product['Sales'],2)
 total_sales_product=formatIndex(total_sales_product)
-
+fig=df.bar(total_sales_product,x='Product Name',y='Sales')
+st.plotly_chart(fig,use_container_width=True)
 ##add the format index option
